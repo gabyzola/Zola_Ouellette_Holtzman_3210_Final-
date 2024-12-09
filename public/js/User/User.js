868a90969ea9a0e094818442a23145abefe38915
SethMC26 <holtzmans@merrimack.edu>
2024-12-09 17:48:40 -0500

    Feat: Started to implement animatiosn

--- public/js/User/User.js
+++ public/js/User/User.js
@@ -9,7 +9,7 @@ export default class User extends THREE.Group {
         this.headMesh = null;
         this.bodyMesh = null;
 
-        
+        this.jumpsize = 30; 
     }
 
     /**
@@ -41,8 +41,6 @@ export default class User extends THREE.Group {
         bodyMesh.receiveShadow = true;
         bodyMesh.castShadow = true;
         this.add(this.bodyMesh);
-
-        
     }
 
     /**
@@ -52,4 +50,46 @@ export default class User extends THREE.Group {
         this.boundingBox = new THREE.Box3().setFromObject(this);
     }
 
+    addAnimations() {
+         this.mixer = new THREE.AnimationMixer(this);
+         //Create Axis for Quaternion angle 
+         let xAxis = new THREE.Vector3(1, 0, 0);
+         //create final rotation quaternion 
+         let qFinal = new THREE.Quaternion().setFromAxisAngle(xAxis, Math.PI * 2);
+         //add animations clips p1 move to center
+         let clip = this._createAnimationClip(
+             [
+                 this.position.x, this.position.y, this.position.z,
+                 this.position.x - this.jumpsize/2, this.position.y + 5 , this.position.z,
+                 this.position.x - this.jumpsize, this.position.y, this.position.z,
+
+             ],
+             [
+                 this.quaternion.x, this.quaternion.y, this.quaternion.z, this.quaternion.w,
+                 qFinal.x, qFinal.y, qFinal.z, qFinal.w,
+                 this.quaternion.x, this.quaternion.y, this.quaternion.z, this.quaternion.w
+             ]
+         )
+         //add clip to create clip
+         this.moveAnimation = this.mixer.clipAction(clip)
+         //only loop once 
+         //this.moveAnimation.loop = THREE.LoopOnce
+         //this.moveAnimation.clampWhenFinished = true;
+    }
+
+    /**
+     * Creates new animation clip 
+     * 
+     * @param {Array} positionArray Array of lenght 9 with positions for animation
+     * @param {Array} quaternionArray Array of legnth 9 with rotations as Quaternions
+     * @returns {THREE.AnimationClip} a new animation clip
+     */
+    _createAnimationClip(positionArray, quaternionArray) {
+        let quaternionKF = new THREE.QuaternionKeyframeTrack('.quaternion', [0, 1, 2], quaternionArray);
+        let position = new THREE.VectorKeyframeTrack('.position', [0, 1, 2], positionArray)
+
+        return new THREE.AnimationClip('action', 3, [position, quaternionKF])
+    }
+
+    
 }
